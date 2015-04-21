79c7f6dbc66deb2e664cf1760355bef0874e4eba
Xibo Ning <xning@redhat.com>
2015-04-21 09:36:45 +0800

    Add functions to read and write Firefox localstorage

--- functions.sh
+++ functions.sh
@@ -1,5 +1,9 @@
-# Requirements
-# perl perl-Config-IniFiles redhat-lsb-core
+# Requirements (package names on CentOS)
+# perl perl-Config-IniFiles jq sqlite3 perl(DBI) perl(DBD::SQLite)
+# sqlite3 need more latest version or we cannot access the sqlites
+# owned by Firefox.
+
+# Functions to get xul/Firefox pathes
 function wait_a_while {
     sleep 3
 }
@@ -15,30 +19,15 @@ function get_home_dir {
     )
 }
 
-function get_path_to_mozilla_xremote_client {
-    (
-        prog=''
-        for dir in {/usr/lib,/usr/lib64}/firefox{,/xulrunner} {/usr/lib,/usr/lib64}/xulrunner-*;do
-            if [ -d ${dir} ];then
-                prog=${dir}/mozilla-xremote-client
-                [ -x ${prog} ] && { echo ${prog};return 0; }
-            fi
-        done
-        os=$(lsb_release --id | cut -s -d':' -f 2 | sed 's/^\s*//g' | sed 's/\s*$//g')
-        echo "Now do not support ${os}."
-        echo "If you know the path of mozilla-xremote-client, pls send email to the maintainer."
-        echo "Perhaps you can submit a patch."
-        exit 1
-    )
-}
-
 function get_firefox_db_dir {
     (
         section="$1"
         ret=0
         home=$(get_home_dir)
         if [ -e ${home}/.mozilla/firefox/profiles.ini ];then
-            dir=$(perl -E "use Config::IniFiles;say Config::IniFiles->new(-file => q{${home}/.mozilla/firefox/profiles.ini})->val(q{${section:-Profile0}}, q{Path});")
+            dir=$(perl -E "\
+                    use Config::IniFiles;\
+                    say Config::IniFiles->new(-file => q{${home}/.mozilla/firefox/profiles.ini})->val(q{${section:-Profile0}}, q{Path});")
             dir=${home}/.mozilla/firefox/${dir}
         else
             dir=''
@@ -49,26 +38,29 @@ function get_firefox_db_dir {
     )
 }
 
-function is_firefox_open {
+function make_sure_firefox_db_dir {
     (
         dir=$(get_firefox_db_dir)
-        fuser -s ${dir}/.parentlock
+        if [ "${dir}"x = x ];then
+            open_firefox_win
+            wait_a_while
+            close_firefox_win
+            get_firefox_db_dir
+        else
+            echo $dir
+        fi
+        
+        return $(true && echo $?)
     )
 }
 
-function close_firefox_win {
-    (
-        dir=$(get_firefox_db_dir)
-        fuser -s -k ${dir}/.parentlock && wait_a_while
-    )
-}
-
+# Functions to control Firefox
 function set_firefox_user_pref {
     (
         key=${1:-};
         value=${2:-}
         if [ "${key}"x = x ];then
-            echo "Need at least one argument"
+            echo "Need at least one argument" 1>&2
             exit $(false || echo $?)
         fi
         [ "${value}"x = x ] && value='false'
@@ -88,7 +80,7 @@ function set_firefox_user_pref_in_mass {
     (
         value=${1:-};
         if [ "${value}"x = x ];then
-            echo "Need at least one argument"
+            echo "Need at least one argument" >&2
             exit $(false || echo $?)
         fi
         shift
@@ -158,23 +150,51 @@ function set_firefox_no_password_remember {
 function open_firefox_win {
     (
         uri=$1
-        nohup firefox ${uri:-} &
-        wait_a_while
+        d=$(mktemp)
+        o=$(pwd)
+        cd $d
+        is_firefox_open && close_firefox_win
+        { nohup firefox --new-instance ${uri:-} & } 2>&1 > /dev/null
+        for ((i = 0; i < 60; i++));do
+            if is_firefox_open;then
+                break
+            else
+                wait_a_while
+            fi
+        done
+        rm -rf $d
+        cd ${0}
     )
 }
 
 function open_firefox_tab {
     (
         uri=$1
-        prog_full_path=$(get_path_to_mozilla_xremote_client)
-        cd $(dirname ${prog_full_path})
-        prog=$(basename ${prog_full_path})
-        ./${prog} -a firefox "openURL(http://redhat.com, new-tab)"
+        { firefox --new-tab "${uri:-http://redhat.com}"; } 2>&1 > /dev/null
         wait_a_while
-        
     )
 }
 
+function is_firefox_open {
+    (
+        dir=$(get_firefox_db_dir)
+        if [ "${dir}"x = x ];then
+            return $(false || echo $?)
+        else
+            fuser -s ${dir}/.parentlock
+        fi
+    )
+}
+
+function close_firefox_win {
+    (
+        dir=$(get_firefox_db_dir)
+        { fuser -s -k ${dir}/.parentlock && wait_a_while; } 2>&1 > /dev/null
+        rm -f ${dir}/.parentlock
+    )
+}
+
+# Functions to control SSL/TSL certificates
 function get_ssl_cert_from_remote {
     (
         host=$1
@@ -236,6 +256,186 @@ function remove_cert_from_db {
     )
 }
 
+# Functions to read/write localStorage/sessionStorage.
+# https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Storage
+# {scope, key} must be unique in the localStorage/sessoinStorage
+function quote_str_for_sqlite3 {
+    (
+        str="$1"
+        tmp_db=$(mktemp)
+        perl -E "\
+              use DBI;\
+              my \$dbh = DBI->connect(q[dbi:SQLite:dbname=${tmp_db}], \
+                 q{}, q{}, { RaiseError => 1, AutoCommit => 0 });\
+              say \$dbh->quote(q{${str}});\
+        "
+        rm -f ${tmp_db}
+    )
+}
+
+function get_path_to_localstorage {
+    (
+        dir=$(make_sure_firefox_db_dir)
+        dir="${dir}/webappsstore.sqlite"
+        if [ -r "${dir}" ];then
+            echo "${dir}"
+            return $(true && echo $?)
+        else
+            echo ''
+            return $(false || echo $?)
+        fi
+    )
+}
+
+function read_value_from_localstorage {
+    (
+        key="$1"
+        scope="$2"
+        
+        if [ "${key}"x = x ];then
+            echo "read_from_localstorage need at least an argument as the key"
+            return $(false || echo $?)
+        fi
+        
+        db=$(get_path_to_localstorage)
+        tbl=webappsstore2
+
+        key=$(quote_str_for_sqlite3 "${key}")
+        if [ "${scope}"x != x ];then
+            scope=$(quote_str_for_sqlite3 "${scope}")
+            where_clause="WHERE scope = ${scope} AND key = ${key}"  
+        else
+            where_clause="WHERE key = ${key}"
+        fi
+        select_clause="SELECT value FROM ${tbl} ${where_clause}"
+        sqlite3 -batch -noheader ${db} "${select_clause}"
+    )
+}
+
+function read_secure_from_localstorage {
+    (
+        key="$1"
+        scope="$2"
+        if [ "${key}"x = x ];then
+            echo "read_from_localstorage need at least an argument as the key"
+            return $(false || echo $?)
+        fi
+        
+        db=$(get_path_to_localstorage)
+        tbl=webappsstore2
+
+        key=$(quote_str_for_sqlite3 "${key}")
+        if [ "${scope}"x != x ];then
+            scope=$(quote_str_for_sqlite3 "${scope}")
+            where_clause="WHERE scope = ${scope} AND key = ${key}"  
+        else
+            where_clause="WHERE key = ${key}"
+        fi
+        select_clause="SELECT secure FROM ${tbl} ${where_clause}"
+        sqlite3 -batch -noheader ${db} "${select_clause}"
+    )
+}
+
+function read_owner_from_localstorage {
+    (
+        key="$1"
+        scope="$2"
+        if [ "${key}"x = x ];then
+            echo "read_from_localstorage need at least an argument as the key"
+            return $(false || echo $?)
+        fi
+        
+        db=$(get_path_to_localstorage)
+        tbl=webappsstore2
+
+        key=$(quote_str_for_sqlite3 "${key}")
+        if [ "${scope}"x != x ];then
+            scope=$(quote_str_for_sqlite3 "${scope}")
+            where_clause="WHERE scope = ${scope} AND key = ${key}"  
+        else
+            where_clause="WHERE key = ${key}"
+        fi
+        select_clause="SELECT owner FROM ${tbl} ${where_clause}"
+        sqlite3 -batch -noheader ${db} "${select_clause}"
+    )
+}
+
+function delete_from_localstorage {
+    (
+        key="$1"
+        scope="$2"
+        if [ "${key}"x = x ];then
+            echo "delete_from_localstorage need at least an argument as follows"
+            echo "delete_from_localstorage key [scope]"
+            return $(false || echo $?)
+        fi
+        
+        db="$(get_path_to_localstorage)"
+        tbl='webappsstore2'
+
+        key=$(quote_str_for_sqlite3 "${key}")
+        if [ "${scope}"x != x ];then
+            scope=$(quote_str_for_sqlite3 "${scope}")
+            where_clause="WHERE scope = ${scope} AND key = ${key}"  
+        else
+            where_clause="WHERE key = ${key}"
+        fi
+        
+        sqlite3 ${db} "DELETE FROM ${tbl} ${where_clause}"
+    )
+}
+
+# We will first delete, then write.
+# Here value could be a file path, then we will read it
+function write_to_localstorage {
+    (
+        scope="$1"
+        key="$2"
+        value="$3"
+        secure="$4"
+        owner="$5"
+        if [ "${scope}"x = x -o "${key}"x = x -o "${value}x" = x ];then
+            echo "write_to_localstorage need at least three arguments as follows"
+            echo "write_to_localstorage scope key value [secure] [owner]"
+            return $(false || echo $?)
+        fi
+
+        if [ -r "${value}" -a -f "${value}" ];then
+            file="${value}"
+            if eval "jq -M -c '.' \"${file}\" >/dev/null 2>&1";then
+                value=$(jq -M -c '.' "${file}")
+            else
+                echo "jq cannnot parse the file $file" 1>&2
+                exit $(false || echo $?)
+            fi
+        fi
+        if eval "jq -n -M -c '@sh | ${value}' > /dev/null 2>&1";then
+            value=$(jq -n -M -c "@sh | ${value}")
+        else
+            echo "write_to_localstorage: cannot covert the value to JSON" 1>&2
+            echo $value 1>&2
+            exit $(false || echo $?)
+        fi
+        scope=$(quote_str_for_sqlite3 "${scope}")
+        key=$(quote_str_for_sqlite3 "${key}")
+        value=$(quote_str_for_sqlite3 "${value}")
+        if [ "${secure}"x != x ];then
+            secure=$(quote_str_for_sqlite3 "${secure}")
+        fi
+        if [ "${owner}"x != x ];then
+            owner=$(quote_str_for_sqlite3 "${owner}")
+        fi
+        db="$(get_path_to_localstorage)"
+        tbl='webappsstore2'
+        
+        delete_clause="DELETE FROM ${tbl} WHERE scope = ${scope} AND key = ${key}"
+        
+        insert_clause="INSERT INTO ${tbl} (scope, key, value, secure, owner) VALUES"
+        sqlite3 ${db} "${delete_clause};${insert_clause} (${scope}, $key, $value, ${secure:-''}, ${owner:-''})"
+    )
+}
+
+# Firefox need an X window. We could try VNC server for it if need.
 function setting_vnc_password {
     (
         passwd=$1
